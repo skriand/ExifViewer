e539960b28b18418f906b2e2aa2c8e90117d9122
Andrey <andrey.skri2011@gmail.com>
2024-03-10 21:01:50 +0300

    Update Theme.kt

--- app/src/main/java/com/example/exifviewer/ui/theme/Theme.kt
+++ app/src/main/java/com/example/exifviewer/ui/theme/Theme.kt
@@ -57,8 +57,8 @@ fun ExifViewerTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = colorScheme.background.toArgb()
+            window.navigationBarColor = colorScheme.background.toArgb()
         }
     }
 
